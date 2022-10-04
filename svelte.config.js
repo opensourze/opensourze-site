3608d3814e37a1061e9079ab223a362ffe897a60
opensourze <opensourze@protonmail.com>
2022-10-04 18:19:54 +0530

    dont mind me

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
-import adapter from "@sveltejs/adapter-static";
+import netlify from "@sveltejs/adapter-netlify";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
     target: "#svelte",
-    adapter: adapter(),
+    adapter: netlify(),
   },
 };
 
